0dee86fc8cbe163e5d6ac08b6bb41828de6aa232
Velonz15422 <santiago15422@gmail.com>
2023-09-06 22:01:28 -0500

    curso spring

---
+++ form-thymeleaf/src/main/java/com/spring/form/formthymeleaf/FormThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.spring.form.formthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormThymeleafApplication.class, args);
+	}
+
+}
