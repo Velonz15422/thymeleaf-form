bb792ce3e795c7556bdd79bc3d30c9964026bac7
Velonz15422 <santiago15422@gmail.com>
2023-09-08 15:15:11 -0500

    Cambio en mensajes de error y agregar campos

--- form-thymeleaf/src/main/java/com/spring/form/formthymeleaf/controllers/FormController.java
+++ form-thymeleaf/src/main/java/com/spring/form/formthymeleaf/controllers/FormController.java
@@ -6,24 +6,30 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.spring.form.formthymeleaf.models.domain.Usuario;
 
 import jakarta.validation.Valid;
 
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
 
     @GetMapping("/form")
     public String form(Model model) {
         Usuario usuario = new Usuario();
+        usuario.setIdentificador("12345.678-k");
+        usuario.setNombre("Alex");
+        usuario.setApellido("velez");
         model.addAttribute("titulo", "Form");
         model.addAttribute("usuario", usuario);
         return "/form";
     }
 
     @PostMapping("/form")
-    public String procesar(@Valid Usuario usuario , BindingResult result, Model model) {
+    public String procesar(@Valid Usuario usuario , BindingResult result, Model model, SessionStatus status) {
         if(result.hasErrors()){
             //Map<String, String> errores = new HashMap<>();
            // result.getFieldErrors().forEach(err ->{
@@ -35,6 +41,8 @@ public class FormController {
 
         model.addAttribute("titulo", "Resultado Form");
         model.addAttribute("usuario", usuario);
+
+        status.setComplete();
 
         return "resultado";
     }
--- form-thymeleaf/src/main/java/com/spring/form/formthymeleaf/models/domain/Usuario.java
+++ form-thymeleaf/src/main/java/com/spring/form/formthymeleaf/models/domain/Usuario.java
@@ -1,16 +1,55 @@
 package com.spring.form.formthymeleaf.models.domain;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 public class Usuario {
 
+    private String identificador;
+
+    
+    @NotEmpty(message = "Por favor llenar el campo")
+    private String nombre;
+
     @NotEmpty
+    private String apellido;
+
+    @NotEmpty
+    @Size(min = 3, max =12)
     private String username;
+
     @NotEmpty
     private String password;
+
     @NotEmpty
+    @Email
     private String email;
 
+
+    public String getIdentificador() {
+        return this.identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+     public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return this.apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 
     public String getUsername() {
         return this.username;
